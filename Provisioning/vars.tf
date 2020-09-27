86bc6a37f1410ae8be60b571f467b0a81e64f72f
srikanth78 <srikanth.groups@gmail.com>
2020-09-27 13:21:11 +0530

    Update vars.tf

--- Provisioning/vars.tf
+++ Provisioning/vars.tf
@@ -1,7 +1,2 @@
 variable "resource_prefix" { type = string }
 variable "location" { type = string }
-variable "virtual_network_name" { type = string }
-variable "subnet_name" { type = list(string) }
-variable "virtual_network_address_space" { type = string }
-variable "subnet_address_space" { type = list(string) }
-
