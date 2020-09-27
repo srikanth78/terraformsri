4e0014f965fd2172a2c720ea43f1a8e4d7f0af55
srikanth78 <srikanth.groups@gmail.com>
2020-09-27 19:05:31 +0530

    Update main.tf

--- azure-network/rg/main.tf
+++ azure-network/rg/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
   # tags     = var.tags
